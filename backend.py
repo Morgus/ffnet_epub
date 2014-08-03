96ad69bb76e0d82a32f63aae6fec73ce239d8d5a
Aleksi Blinnikka <aleksi.blinnikka@gmail.com>
2014-08-03 23:16:07 +0300

    Exit gracefully with no parser found

--- backend.py
+++ backend.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # 2014 Aleksi Blinnikka
 
-import concurrent.futures, zipfile, importlib, re, queue, os.path
+import concurrent.futures, zipfile, importlib, re, queue
 from threading import BoundedSemaphore
 from urllib.request import urlopen, Request
 from extra_files import *
+try:
+    import _thread
+except ImportError:
+    import _dummy_thread as _thread
 
 PARSERS = { "fanfiction.net": "ff_net" }
 # Invalid filenames on Windows
 INVALID_CHARS = re.compile("[/\\:*?\"<>|]")
 
 dl_semaphore = None
-
 progress_queue = queue.Queue()
 
 def get_chapter(url, chapter_no, parse):
@@ -33,7 +36,8 @@ def get_parser(url):
         if key in url:
             parser = importlib.import_module("parsers.{}".format(PARSERS[key]))
             return parser.parse, parser.parse_ch1
-    raise NoParser(url)
+    progress_queue.put((None, "No parser found for {}".format(repr(url))))
+    _thread.exit()
 
 class NoParser(Exception):
     def __init__(self, url):
@@ -50,12 +54,13 @@ def create_document(main_url, max_connections=2, filepath=None):
            of some of the websites states that you shouldn't cause more stress
            than a normal visitor.
        filepath -- optional path for the resulting Epub document
-           By default filename is: <author> - <title>.epub in the current
-           directory."""
+           By default filename is: %author - %title.epub in the current
+           directory. %author and %title in the path are special, they're
+           changed to the story author and title respectively."""
     global dl_semaphore
     dl_semaphore = BoundedSemaphore(max_connections)
-    parse, parse_ch1 = get_parser(main_url)
 
+    parse, parse_ch1 = get_parser(main_url)
     html_ch1, chapter_num, story_info = get_chapter1(main_url, parse_ch1)
     chapters = {}
     chapters[1] = html_ch1
@@ -63,39 +68,38 @@ def create_document(main_url, max_connections=2, filepath=None):
     if story_info["cover_url"]:
         cover_image_req = \
           Request(story_info["cover_url"], headers=story_info["cover_headers"])
-        story_info["cover_image"] = urlopen(cover_image_req).read()
+        cover_image = urlopen(cover_image_req).read()
     else:
-        story_info["cover_image"] = open("default.jpg", "rb").read()
+        cover_image = open("default.jpg", "rb").read()
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        chs_to_parse = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_connections+3) \
+            as executor:
+        parse_chapters = []
+        download_urls = story_info["chapter_urls"]
         for ch in range(2, chapter_num+1):
             dl_semaphore.acquire()
-            chs_to_parse.append(executor.submit(get_chapter,
-                                        story_info["ch_urls"][ch], ch, parse))
+            parse_chapters.append(executor.submit(get_chapter,
+                                        download_urls[ch], ch, parse))
         for future in concurrent.futures.as_completed(chs_to_parse):
-            html, ch_no = future.result()
-            chapters[ch_no] = html
+            html, chapter_no = future.result()
+            chapters[chapter_no] = html
 
     if not filepath:
         filepath = "{} - {}.epub".format(
                 INVALID_CHARS.sub("-", story_info["author"]),
                 INVALID_CHARS.sub("-", story_info["title"]))
-    elif os.path.isdir(filepath):
-        filepath = os.path.join(filepath, "{} - {}.epub".format(
-                INVALID_CHARS.sub("-", story_info["author"]),
-                INVALID_CHARS.sub("-", story_info["title"])))
     else:
         filepath = filepath.replace("%author",
                 INVALID_CHARS.sub("-", story_info["author"]))
         filepath = filepath.replace("%title",
                 INVALID_CHARS.sub("-", story_info["title"]))
+
     with zipfile.ZipFile(filepath, "w") as f:
         f.writestr("mimetype", MIMETYPE)
         f.writestr("META-INF/container.xml", CONTAINER_XML)
         f.writestr("Content/titlepage.html", TITLEPAGE_HTML)
         f.writestr("Content/styles.css", STYLES_CSS)
-        f.writestr("Content/cover.jpg", story_info["cover_image"])
+        f.writestr("Content/cover.jpg", cover_image)
         f.writestr("Content/toc.ncx", toc(story_info, chapter_num))
         f.writestr("Content/content.opf", contents(story_info, chapter_num))
         for ch in range(1, chapter_num+1):
